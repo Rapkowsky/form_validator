789e1ba43bc8f88afcf379f688469af92458cda7
Rapkowsky <robertrapkowski19@gmail.com>
2022-02-21 22:14:18 +0100

    error handling added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
 const username = document.querySelector("#username");
-const pass = document.querySelector("#password");
-const pass2 = document.querySelector("#password2");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password-repeat");
 const email = document.querySelector("#email");
 const sendBtn = document.querySelector(".send");
 const clearBtn = document.querySelector(".clear");
 const popup = document.querySelector(".popup");
+
+const checkForm = (input) => {
+	input.forEach((el) => {
+		if (el.value === "") {
+			showError(el, el.placeholder);
+		} else {
+			removeError(el);
+		}
+	});
+};
+
+const showError = (input, msg) => {
+	const formBox = input.parentElement;
+	const errorMsg = formBox.querySelector(".error-text");
+	formBox.classList.add("error");
+	errorMsg.textContent = msg;
+};
+
+const removeError = (input) => {
+	const formBox = input.parentElement;
+	formBox.classList.remove("error");
+};
+
+sendBtn.addEventListener("click", (el) => {
+	el.preventDefault();
+
+	checkForm([username, password, password2, email]);
+});
+
+clearBtn.addEventListener("click", (el) => {
+	el.preventDefault();
+
+	[username, password, password2, email].forEach((el) => {
+		el.value = "";
+	});
+});
